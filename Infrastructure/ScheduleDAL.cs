c41fdf3c60a1669c63ce5ea006a0ea17f9abe357
vasooM <vmikovic@yahoo.com>
2018-12-06 21:51:40 +0100

    SecondCommit

--- Infrastructure/ScheduleDAL.cs
+++ Infrastructure/ScheduleDAL.cs
@@ -30,12 +30,12 @@ namespace Tablesheet.Infrastructure
         public DataTable ReadNextShiftByUsername(string username)
         {
             Connection conn = new Connection();
-            string query = string.Format(@"select Date, StartShift, EndShift 
+            string query = string.Format(@"select TOP 1 Date, StartShift, EndShift 
                                     from Schedule join Employee on Schedule.EmployeeID = Employee.ID 
                                     join UserLogin on UserLogin.ID = Employee.UserLoginID
                                     where UserLogin.Username = @username and 
-                                    Date < (select MAX(Date) from Schedule)
-                                    order by Date desc");
+                                    Date >= GETDATE()
+                                    order by Date");
             SqlParameter[] sqlParameters = new SqlParameter[1];
             sqlParameters[0] = new SqlParameter("@username", SqlDbType.VarChar);
             sqlParameters[0].Value = Convert.ToString(username);
--- StoredProcClientName.sql
+++
@@ -1,8 +0,0 @@
-Create proc st_jobCode
-@jobCode varchar(10), 
-@Name varchar(50) output
-as
-select Name from Client 
-join Job on Client.ID = Job.ClientID
-where Job.Code = @jobCode
-
